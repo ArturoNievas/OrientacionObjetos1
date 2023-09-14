1f32b2539eaebbc52107ceaf18835408b3bc5a77
ArturoNievas <99201513+ArturoNievas@users.noreply.github.com>
2023-09-14 13:25:09 -0300

    Update Circulo.java

--- figurasYCuerpos/src/main/java/ar/edu/info/oo1/ej4_figuras/Circulo.java
+++ figurasYCuerpos/src/main/java/ar/edu/info/oo1/ej4_figuras/Circulo.java
@@ -3,20 +3,17 @@ package ar.edu.info.oo1.ej4_figuras;
 public class Circulo extends Figura2D {
 
 	private double radio;
-	private double diametro;
 	
 	public double getRadio() {
-		return radio;
+		return this.radio;
 	}
 	public void setRadio(double radio) {
 		this.radio = radio;
-		this.diametro = radio*2;
 	}
 	public double getDiametro() {
-		return diametro;
+		return this.radio*2;
 	}
 	public void setDiametro(double diametro) {
-		this.diametro = diametro;
 		this.radio = diametro/2;
 	}
 	
