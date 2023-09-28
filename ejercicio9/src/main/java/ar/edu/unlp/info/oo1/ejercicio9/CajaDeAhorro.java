7909a7f978b94164b8158778a18b46770cf2f12f
ArturoNievas <99201513+ArturoNievas@users.noreply.github.com>
2023-09-28 13:05:27 -0300

    Update CajaDeAhorro.java

--- ejercicio9/src/main/java/ar/edu/unlp/info/oo1/ejercicio9/CajaDeAhorro.java
+++ ejercicio9/src/main/java/ar/edu/unlp/info/oo1/ejercicio9/CajaDeAhorro.java
@@ -6,15 +6,8 @@ public class CajaDeAhorro extends Cuenta {
 		super();
 	}
 
-	@Override
-	public boolean extraer(double monto) {
-		return super.extraer(monto*1.02);
+	protected void extraerSinControlar(double monto){
+		super.extraerSinControlar(monto*1.02);
 	}
-
-	@Override
-	public void depositar(double monto) {
-		super.depositar(monto*0.98);
-	}
-
 
 }
